f5b17fae7e9059c5e438c562e571f6a99c629d5a
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-06-02 20:42:31 +0300

    added item

--- src/main/java/com/example/ravemod/RaveMod.java
+++ src/main/java/com/example/ravemod/RaveMod.java
@@ -1,10 +1,12 @@
 package com.example.ravemod;
 
+import com.example.ravemod.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -28,7 +30,10 @@ public class RaveMod
     public RaveMod()
     {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        ModItems.register(eventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/com/example/ravemod/item/ModItems.java
@@ -0,0 +1,19 @@
+package com.example.ravemod.item;
+
+import com.example.ravemod.RaveMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RaveMod.MOD_ID);
+
+    public static final RegistryObject<Item> JANK = ITEMS.register("jank", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
